0ff8bb9d71660aa06030f0561957f5cb306b4cef
tmarcum <marcum.taylor@gmail.com>
2024-09-04 18:48:22 -0700

    Add project files.

---
+++ engine.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+using namespace std;
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Engine", NULL, NULL);
+
+	if (window == NULL)
+	{
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		cout << "Failed to initialize GLAD" << endl;
+		return -2;
+	}	glViewport(0, 0, 800, 600);	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);	while (!glfwWindowShouldClose(window))
+	{
+		//Input
+		processInput(window);
+		//Rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//Check events and swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}	glfwTerminate();
+	return 0;
+}
