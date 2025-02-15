b66cf4f830dc144a9cd309687e72117f6a839d94
tmarcum <marcum.taylor@gmail.com>
2025-02-15 09:17:41 -0800

    entity component system implementation

---
+++ libs/includes/eventmanager.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "event.h"
+#include "types.h"
+#include <functional>
+#include <list>
+#include <unordered_map>
+
+class EventManager
+{
+public:
+	void AddListener(EventId eventId, std::function<void(Event&)> const& listener)
+	{
+		listeners[eventId].push_back(listener);
+	}
+
+	void SendEvent(Event& event)
+	{
+		uint32_t type = event.GetType();
+
+		for (auto const& listener : listeners[type])
+		{
+			listener(event);
+		}
+	}
+
+	void SendEvent(EventId eventId)
+	{
+		Event event(eventId);
+
+		for (auto const& listener : listeners[eventId])
+		{
+			listener(event);
+		}
+	}
+
+private:
+	std::unordered_map<EventId, std::list<std::function<void(Event&)>>> listeners;
+};
